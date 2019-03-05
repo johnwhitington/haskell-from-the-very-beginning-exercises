690c3899fdec96f94b272b96982d64500b3ec752
John Whitington <john@coherentgraphics.co.uk>
2019-03-05 17:27:04 +0000

    more

--- exercises/Chapter7/Exercises.hs
+++ exercises/Chapter7/Exercises.hs
@@ -20,9 +20,9 @@ smallest0 l =
     Just a -> a
 
 
-sqrt' :: (Num a, Ord a) => a -> Maybe a
+sqrtMaybe :: (Num a, Ord a) => a -> Maybe a
 
-sqrt' n =
+sqrtMaybe n =
   if n < 0 then Nothing else Just (s 1 n)
     where
       s x n = if x * x > n then x - 1 else s (x + 1) n
